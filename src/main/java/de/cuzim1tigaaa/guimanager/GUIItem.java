895fbdc040a40767e6beefabfafb348c6991cbb6
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2025-02-28 14:54:04 +0100

    Better getting objects by index

--- src/main/java/de/cuzim1tigaaa/guimanager/GUIItem.java
+++ src/main/java/de/cuzim1tigaaa/guimanager/GUIItem.java
@@ -5,5 +5,11 @@ import org.bukkit.inventory.Inventory;
 /**
  * Useful for managing guis with multiple pages
  * Can store all types of objects for executing different tasks
+ * and storing information across multiple pages and different guis
  */
-public record GUIItem(Inventory inventory, int page, Object... objects) {}
+public record GUIItem(Inventory inventory, int page, Object... objects) {
+
+    public Object getObject(int index) {
+        return objects[index];
+    }
+}
